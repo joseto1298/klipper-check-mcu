2c73325df3d882fe16a65d40cde2b3eab6678c3a
joseto1298 <joseto1298@gmail.com>
2026-06-09 19:16:26 +0200

    Fix: grep for /dev/serial/by-id/ to skip host MCU (tmp)

--- check_mcu.sh
+++ check_mcu.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 # check_mcu.sh - Bloquea el arranque de Klipper si el MCU de la impresora no está presente
 
-MCU=$(grep "^serial:" "$HOME/printer_data/config/printer.cfg" | head -1 | sed 's/^serial:[[:space:]]*//')
+MCU=$(grep "/dev/serial/by-id/" "$HOME/printer_data/config/printer.cfg" | head -1 | sed 's/^serial:[[:space:]]*//')
 
 if [ -z "$MCU" ] || [ ! -e "$MCU" ]; then
-    exit 1
-fi
-
-if ! timeout 1 head -c 1 "$MCU" > /dev/null 2>&1; then
     exit 1
 fi
 
